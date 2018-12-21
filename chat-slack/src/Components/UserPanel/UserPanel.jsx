0200efe1b4a37bee4a49e465a901c1b0d88ac1e7
Vadym Pichurin <vadympichurin@gmail.com>
2018-12-21 13:47:52 +0200

    header-markup

---
+++ chat-slack/src/Components/UserPanel/UserPanel.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import { Grid, Header, Icon, Dropdown } from 'semantic-ui-react';
+
+class UserPanel extends Component {
+
+    dropdownOptions = () => [
+        {
+            key: 'user',
+            text: <span><Icon name='sign-in'/> Signed in as <strong>User</strong></span>,
+            disabled: true,
+        },
+        {
+            key: 'avatar',
+            text: <span><Icon name='picture'/> Change Avatar</span>
+        },
+        {
+            key: 'out',
+            text: <span><Icon name='sign-out' />Sign Out</span>
+        }
+    ]
+
+
+    render(){
+        return(
+            <Grid style={{background: "4c3c4c"}}>
+            <Grid.Column>
+                <Grid.Row style={{padding: '1.2rem', margin:'0'}}>
+                <Header inverted floated='left' as='h2'>
+                <Icon name='cloud'/>
+                <Header.Content>Slack clone</Header.Content>
+                </Header>
+                </Grid.Row>
+                <Header style={{padding: '0.25rem'}} as='h4' inverted>
+                <Dropdown trigger={
+                    <span>User</span>
+                } options={this.dropdownOptions()}/>
+                </Header>
+            </Grid.Column>
+            </Grid>
+        )
+    }
+}
+
+export default UserPanel;
