0f2cd99b3f7768f1608e962c58692a4b62f8d827
Vadym Pichurin <vadympichurin@gmail.com>
2018-12-21 15:45:22 +0200

    add channels

--- chat-slack/src/Components/SidePanel/SidePanel.jsx
+++ chat-slack/src/Components/SidePanel/SidePanel.jsx
@@ -1,13 +1,17 @@
 import React, { Component } from "react";
 import { Menu } from 'semantic-ui-react';
 import UserPanel from "../UserPanel/UserPanel";
+import Channels from '../Channels/Channels';
 
 class SidePanel extends Component {
     render() {
         return (
             <Menu size='large' inverted fixed='left' vertical style={{background: '#4c3c4c', fontSize: '1.2rem'}}>
             <UserPanel/>
+            <Channels/>
             </Menu>
+            
+
         )
     }
 }
