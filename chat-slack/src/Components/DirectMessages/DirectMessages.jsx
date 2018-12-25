82a63d844193e34e586d71d66dfe1fe26fe5425a
Vadym Pichurin <vadympichurin@gmail.com>
2018-12-25 16:17:01 +0200

    added_direct_messages_added_users

--- chat-slack/src/Components/DirectMessages/DirectMessages.jsx
+++ chat-slack/src/Components/DirectMessages/DirectMessages.jsx
@@ -1,24 +1,66 @@
-import React, { Component } from 'react';
-import { Menu, Icon } from 'semantic-ui-react';
+import React, { Component } from "react";
+import { Menu, Icon } from "semantic-ui-react";
+import firebase from "../../firebase";
+import { connect } from "react-redux";
 
 class DirectMessages extends Component {
-    state = {
-        users: [],
-    }
+  state = {
+    users: [],
+    usersRef: firebase.database().ref("users")
+  };
 
-    render(){
-        const { users } = this.state;
-
-        return (
-            <Menu.Menu className='menu'>
-            <Menu.Item>
-                <span>
-                    <Icon name='mail' /> DIRECT MESSAGES
-                </span> ({ users.length })
-            </Menu.Item>
-            </Menu.Menu>
-        )
+  componentDidMount() {
+    if (this.props.user) {
+      this.addListener(this.props.user.uid);
     }
+  }
+
+  addListener = id => {
+    let loadedUser = [];
+    this.state.usersRef.on("child_added", snap => {
+      if (id !== snap.key) {
+        let user = snap.val();
+        user.uid = snap.key;
+        user.status = "offline";
+        loadedUser.push(user);
+        this.setState({
+          users: loadedUser
+        });
+      }
+    });
+  };
+
+  // ===============================================================================
+  render() {
+    const { users } = this.state;
+
+    return (
+      <Menu.Menu className="menu">
+        <Menu.Item>
+          <span>
+            <Icon name="mail" /> DIRECT MESSAGES
+          </span>{" "}
+          ({users.length})
+        </Menu.Item>
+
+        {users.map(el => (
+          <Menu.Item
+            key={el.uid}
+            onClick={() => console.log(el)}
+            style={{ opacity: 0.7, fontStyle: "italic" }}
+          >
+            <Icon name="circle" /> @ {el.name}
+          </Menu.Item>
+        ))}
+      </Menu.Menu>
+    );
+  }
 }
 
-export default DirectMessages;
+function MSTP(state) {
+  return {
+    user: state.user.currentUser
+  };
+}
+
+export default connect(MSTP)(DirectMessages);
